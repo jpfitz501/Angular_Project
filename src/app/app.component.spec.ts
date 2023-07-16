b6669826c159172942f142817f3708258ada0d08
John Fitzsimmons <jpfitz501@gmail.com>
2023-07-16 11:30:47 -0400

    Initial commit of Angular Project Repository -John

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'Car_Project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Car_Project');
+    expect(app.title).toEqual('John\'s Used Cars');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Car_Project app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome to John\'s Used Cars!');
   });
 });
